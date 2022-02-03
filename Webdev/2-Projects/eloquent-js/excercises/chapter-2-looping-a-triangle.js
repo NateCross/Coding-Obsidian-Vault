74c6f11652b64e7bef491c30cc97024aa877f984
NateCross <jonathan.a.b.cross@gmail.com>
2022-02-03 15:01:05 +0800

    vault backup: 2022-02-03 15:01:05
    Affected files:
    Webdev/2-Projects/eloquent-js/excercises/chapter-2-looping-a-triangle.js

--- Webdev/2-Projects/eloquent-js/excercises/chapter-2-looping-a-triangle.js
+++ Webdev/2-Projects/eloquent-js/excercises/chapter-2-looping-a-triangle.js
@@ -2,6 +2,20 @@
   Write a loop that makes seven calls to
   console.log to output the following triangle:
 */
-for (let i = 0; i < 7; i++) {
 
+// Basic solution
+for (let i = 1; i <= 7; i++) {
+    let triangle = "";
+    for (let j = 1; j <= i; j++) {
+        triangle += "#";
+    }
+    console.log(triangle);
+}
+
+console.log("---OTHER METHOD---");
+
+// Using String.repeat()
+const triangleString = "#";
+for (let i = 1; i <= 7; i++) {
+    console.log(triangleString.repeat(i));
 }
