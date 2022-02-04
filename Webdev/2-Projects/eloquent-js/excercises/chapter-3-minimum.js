768b301bdba52125ca3cf0a49877a761a2867fc1
NateCross <jonathan.a.b.cross@gmail.com>
2022-02-04 12:52:36 +0800

    vault backup: 2022-02-04 12:52:35
    Affected files:
    Webdev/2-Projects/eloquent-js/excercises/chapter-3-minimum.js
    Webdev/2-Projects/eloquent-js/excercises/chapter-3-recursion.js

---
+++ Webdev/2-Projects/eloquent-js/excercises/chapter-3-minimum.js
@@ -0,0 +1,9 @@
+// Write a function "min" that takes two arguments
+// and returns their minimum
+
+const minimum = (x, y) => {
+    return x <= y ? x : y;
+}
+
+console.log(minimum(1, 2));
+console.log(minimum(10, 5));
