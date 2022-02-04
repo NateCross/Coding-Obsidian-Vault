3e49ee2da682a15035a2c8da6bdfbf45c28997f7
NateCross <jonathan.a.b.cross@gmail.com>
2022-02-04 12:57:35 +0800

    vault backup: 2022-02-04 12:57:35
    Affected files:
    Webdev/2-Projects/eloquent-js/excercises/chapter-3-recursion.js

--- Webdev/2-Projects/eloquent-js/excercises/chapter-3-recursion.js
+++ Webdev/2-Projects/eloquent-js/excercises/chapter-3-recursion.js
@@ -1,9 +1,20 @@
 /*
-    Define a recursive function isEven corresponding to this description. The
-    function should accept a single parameter (a positive, whole number) and return
-    a Boolean.
-    Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
-    way to fix this?
+  Define a recursive function isEven corresponding to this description. The
+  function should accept a single parameter (a positive, whole number) and return
+  a Boolean.
+  Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
+  way to fix this?
 */
 
+const isEvenRecursive = num => {
+  if (num == 0)
+    return true;
+  else if (num == 1)
+    return false;
+  else
+    return isEvenRecursive(num - 2);
+}
 
+console.log(isEvenRecursive(50));
+console.log(isEvenRecursive(75));
+console.log(isEvenRecursive(-1));
